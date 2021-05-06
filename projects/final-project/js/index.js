bd59ae16dcdd0364c5a262737814736b225b3027
lmazouin <53606603+LMazouin@users.noreply.github.com>
2021-05-07 00:47:47 +0200

    update task manager and hat game

--- projects/final-project/js/index.js
+++ projects/final-project/js/index.js
@@ -1,46 +1,93 @@
+// moduleimport validator from 'validator';
+
 // global variables
 const form = document.querySelector('form');
+
+const generalValidator = (str, type) => {
+	if (type === 'name') {
+		return validator.isAlpha(str);
+	}
+	if (type === 'text') {
+		return validator.isAlphanumeric(str);
+	}
+	if (type === 'date') {
+		return validator.isDate(str);
+	}
+	if (type === 'date-after') {
+		return validator.isAfter(str);
+	}
+};
+
+// function to export data from input fields
+const getInputValues = (inputField) => {
+	const input = {};
+	input.content = inputField.value.toString();
+	input.id = inputField.id;
+	input.valid = generalValidator(input.content, inputField.dataset.type);
+	return input;
+};
 
 // create a task manager object
 const taskManager = new TaskManager();
 
 const getTaskName = () => {
 	const taskNameInput = document.getElementById('task-name');
-	const taskName = taskNameInput.value.toLowerCase();
-	taskNameInput.value = '';
-	return taskName;
+	const taskName = taskNameInput.value;
+	if (validator.isAlphanumeric(taskName)) {
+		taskNameInput.value = '';
+		return taskName;
+	} else {
+		return false;
+	}
 };
 
 const getAssignedTo = () => {
 	const assignedToInput = document.getElementById('assigned-to');
-	const assignedTo = assignedToInput.value.toLowerCase();
-	assignedToInput.value = '';
-	return assignedTo;
+	const assignedTo = assignedToInput.value;
+	if (validator.isAlpha(assignedTo)) {
+		assignedToInput.value = '';
+		return assignedTo;
+	} else {
+		return false;
+	}
 };
 
 const getDueDate = () => {
 	const dueDateInput = document.getElementById('due-date');
 	const dueDate = dueDateInput.value;
-	dueDateInput.value = '';
-	return dueDate;
+	if (validator.isAfter(dueDate)) {
+		dueDateInput.value = '';
+		return dueDate;
+	} else {
+		return false;
+	}
 };
 
 const getTaskDescription = () => {
 	const taskDescriptionInput = document.getElementById('task-description');
 	const taskDescription = taskDescriptionInput.value;
-	taskDescriptionInput.value = '';
-	return taskDescription;
+	if (validator.isAlphanumeric(taskDescription)) {
+		taskDescription.value = '';
+		return taskDescription;
+	} else {
+		return false;
+	}
 };
 
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
 	
 	// console.log('hello');
-  
-	const name = getTaskName();
-	const assignedTo = getAssignedTo();
-	const dueDate = getDueDate();
-	const description = getTaskDescription();
+    
+	const inputList = [];
+	document.querySelector('#popup-form input').forEach((input) => {
+		inputList.push(input);
+	});
+
+	const name = inputList.find((input) => input.id === 'task-name');
+	const assignedTo = inputList.find((input) => input.id === 'assigned-to');
+	const dueDate = inputList.find((input) => input.id === 'due-date');
+	const description = inputList.find((input) => input.id === 'task-description');
 
 	console.log(name);
 	console.log(assignedTo);
